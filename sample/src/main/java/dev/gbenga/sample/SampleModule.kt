3295eb440c3af62bc39e1eb7fa5a2a5b4d47e464
Devmike01 <michaelgbenga91@gmail.com>
2025-01-26 02:50:09 +0000

    Added a demo update

--- sample/src/main/java/dev/gbenga/sample/SampleModule.kt
+++
@@ -1,8 +0,0 @@
-package dev.gbenga.sample
-
-class SampleModule {
-
-    fun Int.add(a: Int): Int{
-        return a + this;
-    }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,4 +22,3 @@ dependencyResolutionManagement {
 rootProject.name = "FanController"
 include(":app")
 include(":turner")
-include(":sample")
