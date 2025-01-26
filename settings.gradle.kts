46ba3a3297813ca63f762af4da33e451ad5fb046
Devmike01 <michaelgbenga91@gmail.com>
2025-01-26 15:01:54 +0000

    Added maven

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,14 +10,18 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven(url ="https://jitpack.io")
+
     }
 }
+
 
 rootProject.name = "FanController"
 include(":app")
